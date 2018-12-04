141c532ed815fa5293b8cbc267e37a240a46690d
Leroy McLean <leroythelegend@gmail.com>
2018-12-04 21:06:31 +1000

    New state interface

---
+++ Scripts/IAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace pcars
+{
+    public interface IAction
+    {
+        void Record();
+        void Display();
+
+        void Process(PacketDecoder packet);
+
+
+        void ChangeState(IGameState state);
+    }
+}
---
+++ Scripts/IGameState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace pcars
+{
+    public interface IGameState
+    {
+        void Record(IAction action);
+        void Display(IAction action);
+
+        void Process(IAction action, PacketDecoder packet);
+
+        void ChangeState(IAction action, IGameState state);        
+    }
+}
